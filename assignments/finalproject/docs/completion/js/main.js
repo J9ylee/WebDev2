70a4a4de2539c0e4589fc5ff5b4efc2a1940d8d9
J9ylee <68120791+J9ylee@users.noreply.github.com>
2025-05-15 00:29:39 -0500

    Delete assignments/finalproject/docs/completion directory

--- assignments/finalproject/docs/completion/js/main.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  const navToggle = document.querySelector('.nav-toggle');
-  const navMenu = document.querySelector('.nav-menu');
-  navToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-  });
-});
